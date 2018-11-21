619c892bc35e4b8c21e6e524be7651819577b730
Seva Safris <seva@safris.org>
2018-11-22 02:21:50 +0700

    Add license

--- opentracing-specialagent/src/main/java/io/opentracing/contrib/specialagent/Util.java
+++ opentracing-specialagent/src/main/java/io/opentracing/contrib/specialagent/Util.java
@@ -1,3 +1,18 @@
+/* Copyright 2018 The OpenTracing Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.opentracing.contrib.specialagent;
 
 import java.io.ByteArrayOutputStream;
@@ -562,7 +577,7 @@ final class Util {
    * <i><b>Note:</b> This is a recursive algorithm, implemented to take
    * advantage of the high performance of callstack registers, but will fail due
    * to a {@link StackOverflowError} if the number of differences between the
-   * first and second specified arrays approaches ~80,000.</i>
+   * first and second specified arrays approaches ~8000.</i>
    *
    * @param <T> Type parameter of array.
    * @param a The first specified array (sorted).
