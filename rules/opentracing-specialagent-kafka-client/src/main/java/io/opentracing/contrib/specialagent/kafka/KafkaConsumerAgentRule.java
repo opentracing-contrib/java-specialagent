f7aa9787103fa0a3a14287e0734349b586e3f7cf
Sergei Malafeev <sergeymalafeev@gmail.com>
2019-03-12 14:25:54 +0800

    Add kafka rule
    Signed-off-by: Sergei Malafeev <sergeymalafeev@gmail.com>

---
+++ rules/opentracing-specialagent-kafka-client/src/main/java/io/opentracing/contrib/specialagent/kafka/KafkaConsumerAgentRule.java
@@ -0,0 +1,62 @@
+/* Copyright 2019 The OpenTracing Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.opentracing.contrib.specialagent.kafka;
+
+import static net.bytebuddy.matcher.ElementMatchers.hasSuperType;
+import static net.bytebuddy.matcher.ElementMatchers.named;
+import static net.bytebuddy.matcher.ElementMatchers.takesArguments;
+
+import io.opentracing.contrib.specialagent.AgentRule;
+import io.opentracing.contrib.specialagent.AgentRuleUtil;
+import java.util.Arrays;
+import net.bytebuddy.agent.builder.AgentBuilder;
+import net.bytebuddy.agent.builder.AgentBuilder.InitializationStrategy;
+import net.bytebuddy.agent.builder.AgentBuilder.RedefinitionStrategy;
+import net.bytebuddy.agent.builder.AgentBuilder.Transformer;
+import net.bytebuddy.agent.builder.AgentBuilder.TypeStrategy;
+import net.bytebuddy.asm.Advice;
+import net.bytebuddy.description.type.TypeDescription;
+import net.bytebuddy.dynamic.DynamicType.Builder;
+import net.bytebuddy.implementation.bytecode.assign.Assigner.Typing;
+import net.bytebuddy.utility.JavaModule;
+
+public class KafkaConsumerAgentRule implements AgentRule {
+  @Override
+  public Iterable<? extends AgentBuilder> buildAgent(final String agentArgs) {
+    return Arrays.asList(new AgentBuilder.Default().
+        with(RedefinitionStrategy.RETRANSFORMATION).
+        with(InitializationStrategy.NoOp.INSTANCE).
+        with(TypeStrategy.Default.REDEFINE).
+        type(named("org.apache.kafka.clients.consumer.KafkaConsumer"))
+        .transform(new Transformer() {
+          @Override
+          public Builder<?> transform(final Builder<?> builder,
+              final TypeDescription typeDescription,
+              final ClassLoader classLoader, final JavaModule module) {
+            return builder
+                .visit(Advice.to(KafkaConsumerAgentRule.class)
+                    .on(named("poll").and(takesArguments(1))));
+          }
+        }));
+  }
+
+  @Advice.OnMethodExit
+  public static void exit(@Advice.Return(typing = Typing.DYNAMIC) Object returned) {
+    if (AgentRuleUtil.isEnabled()) {
+      KafkaAgentIntercept.consumerRecords(returned);
+    }
+  }
+
+}
