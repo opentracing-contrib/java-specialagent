3e1b06c078acb3743db340fd8686246cda36edfc
Sergei Malafeev <sergeymalafeev@gmail.com>
2019-05-02 12:13:46 +0800

    add spring web mvc tests
    Signed-off-by: Sergei Malafeev <sergeymalafeev@gmail.com>

---
+++ rules/opentracing-specialagent-spring-webmvc/src/test/java/io/opentracing/contrib/specialagent/spring/webmvc/SpringMVCConfiguration.java
@@ -0,0 +1,35 @@
+package io.opentracing.contrib.specialagent.spring.webmvc;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * @author Pavol Loffay
+ */
+@EnableWebMvc
+@Configuration
+@Import({TestController.class})
+public class SpringMVCConfiguration extends WebMvcConfigurerAdapter implements
+    ServletContextListener {
+
+  @Override
+  public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+    configurer.enable();
+  }
+
+  @Override
+  public void contextInitialized(ServletContextEvent servletContextEvent) {
+
+  }
+
+  @Override
+  public void contextDestroyed(ServletContextEvent servletContextEvent) {
+
+  }
+}
+
