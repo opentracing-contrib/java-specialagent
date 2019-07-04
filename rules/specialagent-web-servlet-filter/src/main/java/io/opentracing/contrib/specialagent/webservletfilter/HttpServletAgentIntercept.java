94d1dbcb495ec1050b949a6c9a3b32a9f65f3331
Seva Safris <seva@safris.org>
2019-07-04 14:33:10 +0200

    Invoke FilterChain

--- rules/specialagent-web-servlet-filter/src/main/java/io/opentracing/contrib/specialagent/webservletfilter/HttpServletAgentIntercept.java
+++ rules/specialagent-web-servlet-filter/src/main/java/io/opentracing/contrib/specialagent/webservletfilter/HttpServletAgentIntercept.java
@@ -16,6 +16,8 @@
 package io.opentracing.contrib.specialagent.webservletfilter;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.WeakHashMap;
 import java.util.logging.Level;
@@ -33,11 +35,21 @@ import io.opentracing.util.GlobalTracer;
 
 public class HttpServletAgentIntercept {
   public static final Map<ServletContext,TracingFilter> servletContextToFilter = new WeakHashMap<>();
-  public static final FilterChain noopFilterChain = new FilterChain() {
-    @Override
-    public void doFilter(final ServletRequest request, final ServletResponse response) throws IOException, ServletException {
+
+  public static TracingFilter getFilter(final ServletContext servletContext) throws ServletException {
+    TracingFilter filter = servletContextToFilter.get(servletContext);
+    if (filter != null)
+      return filter;
+
+    synchronized (servletContextToFilter) {
+      filter = servletContextToFilter.get(servletContext);
+      if (filter != null)
+        return filter;
+
+      servletContextToFilter.put(servletContext, filter = new TracingProxyFilter(GlobalTracer.get(), servletContext));
+      return filter;
     }
-  };
+  }
 
   public static void enter(final Object thiz, final Object req, final Object res) {
     if (!(thiz instanceof HttpServlet))
@@ -46,17 +58,22 @@ public class HttpServletAgentIntercept {
     try {
       final HttpServlet servlet = (HttpServlet)thiz;
       final ServletContext servletContext = servlet.getServletContext();
-      TracingFilter filter = servletContextToFilter.get(servletContext);
-      if (filter == null) {
-        synchronized (servletContextToFilter) {
-          filter = servletContextToFilter.get(servletContext);
-          if (filter == null) {
-            servletContextToFilter.put(servletContext, filter = new TracingProxyFilter(GlobalTracer.get(), servlet.getServletContext()));
-          }
-        }
-      }
+      final TracingFilter filter = getFilter(servletContext);
 
-      filter.doFilter((ServletRequest)req, (ServletResponse)res, noopFilterChain);
+      final List<TracingFilter> filterChain = new ArrayList<>(servletContextToFilter.values());
+      filter.doFilter((ServletRequest)req, (ServletResponse)res, new FilterChain() {
+        @Override
+        public void doFilter(final ServletRequest request, final ServletResponse response) throws IOException, ServletException {
+          if (filterChain.size() == 0)
+            return;
+
+          final TracingFilter nextFilter = filterChain.remove(filterChain.size() - 1);
+          if (filter != nextFilter)
+            nextFilter.doFilter(request, response, this);
+          else
+            doFilter(request, response);
+        }
+      });
     }
     catch (final Exception e) {
       AgentRule.logger.log(Level.WARNING, e.getMessage(), e);
