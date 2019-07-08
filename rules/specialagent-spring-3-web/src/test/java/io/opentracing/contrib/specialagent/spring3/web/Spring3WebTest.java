f8b6ded8218dd6a623b107c365c3867d65f5a9c0
Sergei Malafeev <sergeymalafeev@gmail.com>
2019-07-08 22:23:06 +0800

    Add Spring 3 Web and Web MVC rules
    Signed-off-by: Sergei Malafeev <sergeymalafeev@gmail.com>

---
+++ rules/specialagent-spring-3-web/src/test/java/io/opentracing/contrib/specialagent/spring3/web/Spring3WebTest.java
@@ -0,0 +1,58 @@
+/* Copyright 2019 The OpenTracing Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.opentracing.contrib.specialagent.spring3.web;
+
+import static org.junit.Assert.assertEquals;
+
+import io.opentracing.contrib.specialagent.AgentRunner;
+import io.opentracing.contrib.specialagent.AgentRunner.Config;
+import io.opentracing.mock.MockTracer;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.web.client.RestTemplate;
+
+@RunWith(AgentRunner.class)
+@Config(isolateClassLoader = false)
+@SuppressWarnings("deprecation")
+public class Spring3WebTest {
+  @Before
+  public void before(final MockTracer tracer) {
+    tracer.reset();
+  }
+
+  @Test
+  public void test(final MockTracer tracer) {
+    final RestTemplate restTemplate = new RestTemplate();
+
+    try {
+      restTemplate.getForEntity("http://localhost:12345", String.class);
+    }
+    catch (final Exception ignore) {
+
+    }
+
+    try {
+      restTemplate.getForObject("http://localhost:12345", String.class);
+    }
+    catch (final Exception ignore) {
+
+    }
+
+    assertEquals(2, tracer.finishedSpans().size());
+  }
+
+}
---
+++ rules/specialagent-spring-3-webmvc/src/test/java/io/opentracing/contrib/specialagent/spring3/webmvc/Spring3WebMvcTest.java
@@ -0,0 +1,73 @@
+/* Copyright 2019 The OpenTracing Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.opentracing.contrib.specialagent.spring3.webmvc;
+
+import static org.junit.Assert.*;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.ServerConnector;
+import org.eclipse.jetty.webapp.WebAppContext;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.http.ResponseEntity;
+
+import io.opentracing.contrib.specialagent.AgentRunner;
+import io.opentracing.contrib.specialagent.AgentRunner.Config;
+import io.opentracing.mock.MockTracer;
+import org.springframework.web.client.RestTemplate;
+
+@RunWith(AgentRunner.class)
+@Config(isolateClassLoader = false)
+public class Spring3WebMvcTest {
+  private static final String CONTEXT_PATH = "/";
+  private static Server jettyServer;
+  private static String url;
+
+  @BeforeClass
+  public static void beforeClass() throws Exception {
+    jettyServer = new Server(0);
+
+    final WebAppContext webApp = new WebAppContext();
+    webApp.setServer(jettyServer);
+    webApp.setContextPath(CONTEXT_PATH);
+    webApp.setWar("src/test/webapp");
+
+    jettyServer.setHandler(webApp);
+    jettyServer.start();
+
+    // jetty starts on random port
+    final int serverPort = ((ServerConnector)jettyServer.getConnectors()[0]).getLocalPort();
+    url = "http://localhost:" + serverPort;
+  }
+
+  @AfterClass
+  public static void afterClass() throws Exception {
+    jettyServer.stop();
+    jettyServer.join();
+  }
+
+  @Test
+  public void test(final MockTracer tracer) {
+      final ResponseEntity<String> responseEntity = new RestTemplate()
+          .getForEntity(url, String.class);
+      assertEquals("test", responseEntity.getBody());
+
+    assertEquals(1, tracer.finishedSpans().size());
+    assertFalse(tracer.finishedSpans().get(0).logEntries().isEmpty());
+  }
+}
