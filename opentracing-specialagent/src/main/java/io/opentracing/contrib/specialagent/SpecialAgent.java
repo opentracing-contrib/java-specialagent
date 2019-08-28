e5d060cd4269ff47e5b5c52d1c0b6fce5ee75001
Seva Safris <seva@safris.org>
2019-08-28 12:47:53 +0200

    [LS-10841] Improve shutdown hook

--- opentracing-specialagent/src/main/java/io/opentracing/contrib/specialagent/SpecialAgent.java
+++ opentracing-specialagent/src/main/java/io/opentracing/contrib/specialagent/SpecialAgent.java
@@ -31,7 +31,6 @@ import java.util.HashSet;
 import java.util.IdentityHashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Properties;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.jar.JarFile;
