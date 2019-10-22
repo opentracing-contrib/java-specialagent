d04ff41e4b55087a04983d084018eb34d8995ac5
Seva Safris <seva@safris.org>
2019-10-22 22:19:21 +0800

    Conform Tomcat tests

--- tests/servlet-tomcat-7.0.65-test/src/test/java/tomcat/TomcatAsyncITest.java
+++ tests/servlet-tomcat-7.0.65-test/src/test/java/tomcat/TomcatAsyncITest.java
@@ -1,13 +1,27 @@
+/* Copyright 2019 The OpenTracing Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package tomcat;
 
-import io.opentracing.contrib.specialagent.TestUtil;
-import io.opentracing.util.GlobalTracer;
 import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.logging.Logger;
+
 import javax.servlet.AsyncContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
@@ -16,11 +30,15 @@ import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 import org.apache.catalina.Context;
 import org.apache.catalina.LifecycleException;
 import org.apache.catalina.core.StandardContext;
 import org.apache.catalina.startup.Tomcat;
 import org.junit.Test;
+
+import io.opentracing.contrib.specialagent.TestUtil;
+import io.opentracing.util.GlobalTracer;
 
 public class TomcatAsyncITest {
   private static final Logger logger = Logger.getLogger(TomcatAsyncITest.class.getName());
@@ -55,7 +73,6 @@ public class TomcatAsyncITest {
       }
     });
 
-
     Tomcat.addServlet(appContext, "helloWorldAsyncServlet", new HttpServlet() {
       private static final long serialVersionUID = 6184640156851545023L;
 
@@ -65,22 +82,21 @@ public class TomcatAsyncITest {
           throw new AssertionError("ERROR: no active span");
 
         final AsyncContext asyncContext = request.startAsync(request, response);
-
         new Thread() {
-
           @Override
           public void run() {
             try {
               if (GlobalTracer.get().activeSpan() == null)
                 throw new AssertionError("ERROR: no active span");
 
-              ServletResponse response = asyncContext.getResponse();
+              final ServletResponse response = asyncContext.getResponse();
               response.setContentType("text/plain");
-              PrintWriter out = response.getWriter();
+              final PrintWriter out = response.getWriter();
               out.println("Async Servlet active span: " + GlobalTracer.get().activeSpan());
               out.flush();
               asyncContext.complete();
-            } catch (Exception e) {
+            }
+            catch (final Exception e) {
               throw new RuntimeException(e);
             }
           }
