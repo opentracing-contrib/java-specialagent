00619f7dd3e423bd7a8782fa048e8fceca67b11d
Seva Safris <seva@safris.org>
2019-10-24 14:07:57 +0800

    [LS-12145] Activate testParallelStream()

--- tests/concurrent-test/src/test/java/concurrent/ConcurrentITest.java
+++ tests/concurrent-test/src/test/java/concurrent/ConcurrentITest.java
@@ -20,8 +20,6 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.stream.IntStream;
 
-import org.junit.Test;
-
 import io.opentracing.Scope;
 import io.opentracing.Span;
 import io.opentracing.contrib.specialagent.TestUtil;
@@ -30,20 +28,54 @@ import io.opentracing.util.GlobalTracer;
 
 public class ConcurrentITest {
   public static void main(final String[] args) throws ExecutionException, InterruptedException {
-    new ConcurrentITest().test();
-  }
+    final Span parent = GlobalTracer.get()
+      .buildSpan("parent")
+      .withTag(Tags.COMPONENT, "parent")
+      .start();
 
-  @Test
-  public void test() throws ExecutionException, InterruptedException {
-    final Span parent = GlobalTracer.get().buildSpan("parent")
-      .withTag(Tags.COMPONENT, "parent").start();
-
-    testExecutor(parent);
     testThread(parent);
-    //testParallelStream(parent);
+    testRunnable(parent);
+    testExecutor(parent);
+    testParallelStream(parent);
 
     parent.finish();
     TestUtil.checkSpan("parent", 1);
+  }
+
+  private static void run() {
+    System.out.println("Active span: " + GlobalTracer.get().activeSpan());
+    if (GlobalTracer.get().activeSpan() == null)
+      throw new AssertionError("ERROR: no active span");
+  }
+
+  private static void testThread(final Span parent) throws InterruptedException {
+    final Thread thread = new Thread() {
+      @Override
+      public void run() {
+        ConcurrentITest.run();
+      }
+    };
+
+    try (final Scope scope = GlobalTracer.get().activateSpan(parent)) {
+      thread.start();
+    }
+
+    thread.join();
+  }
+
+  private static void testRunnable(final Span parent) throws InterruptedException {
+    final Thread thread = new Thread(new Runnable() {
+      @Override
+      public void run() {
+        ConcurrentITest.run();
+      }
+    });
+
+    try (final Scope scope = GlobalTracer.get().activateSpan(parent)) {
+      thread.start();
+    }
+
+    thread.join();
   }
 
   private static void testExecutor(final Span parent) throws ExecutionException, InterruptedException {
@@ -62,33 +94,15 @@ public class ConcurrentITest {
     }
   }
 
-  private static void testThread(final Span parent) throws InterruptedException {
-    final Thread thread = new Thread(new Runnable() {
-      @Override
-      public void run() {
-        System.out.println("Active span: " + GlobalTracer.get().activeSpan());
-        if (GlobalTracer.get().activeSpan() == null)
-          throw new AssertionError("ERROR: no active span");
-      }
-    });
-
-    try (final Scope scope = GlobalTracer.get().activateSpan(parent)) {
-      thread.start();
-    }
-
-    thread.join();
-  }
-
   private static void testParallelStream(final Span parent) {
     try (final Scope scope = GlobalTracer.get().activateSpan(parent)) {
       final int sum = IntStream.range(1, 10)
         .parallel()
         .map(operand -> {
-          // TODO: here should be active span
-          System.out.println("Thread: " + Thread.currentThread().getName() +
-              " Active span: " + GlobalTracer.get().activeSpan());
-          return operand * 2;
-        }).sum();
+            // TODO: here should be active span
+            System.out.println("Thread: " + Thread.currentThread().getName() + " Active span: " + GlobalTracer.get().activeSpan());
+            return operand * 2;
+          }).sum();
 
       System.out.println("Sum: " + sum);
     }
