94e122aa6122bb5e7951d129121e7fa863954397
Seva Safris <seva@safris.org>
2019-10-31 14:57:25 +0800

    Add JMS integration tests

---
+++ test/jms-1/src/test/java/io/opentracing/contrib/specialagent/test/jms1/Jms1ITest.java
@@ -0,0 +1,142 @@
+/* Copyright 2019 The OpenTracing Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.opentracing.contrib.specialagent.test.jms1;
+
+import java.util.concurrent.CountDownLatch;
+
+import javax.jms.Connection;
+import javax.jms.DeliveryMode;
+import javax.jms.Destination;
+import javax.jms.ExceptionListener;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageConsumer;
+import javax.jms.MessageProducer;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+import io.opentracing.contrib.specialagent.TestUtil;
+
+public class Jms1ITest {
+  private static final int threadCount = 4;
+  private static final CountDownLatch latch = new CountDownLatch(threadCount);
+
+  public static void main(final String[] args) throws Exception {
+    final ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory("vm://localhost?broker.persistent=false");
+    final Connection connection = connectionFactory.createConnection();
+    connection.start();
+
+    for (int i = 0; i < threadCount; ++i)
+      new Thread(new HelloWorldProducer(connection)).start();
+
+    latch.await();
+    TestUtil.checkSpan("java-jms", 4);
+    connection.close();
+  }
+
+  public static class HelloWorldProducer implements Runnable {
+    private final Connection connection;
+
+    public HelloWorldProducer(final Connection connection) {
+      this.connection = connection;
+    }
+
+    @Override
+    public void run() {
+      try {
+        // Create a Session
+        final Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+        System.out.println("Session: " + session);
+
+        // Create the destination (Topic or Queue)
+        final Destination destination = session.createQueue("TEST.FOO");
+
+        // Create a MessageProducer from the Session to the Topic or Queue
+        final MessageProducer producer = session.createProducer(destination);
+        producer.setDeliveryMode(DeliveryMode.NON_PERSISTENT);
+
+        System.out.println("PRODUCER: " + producer);
+
+        // Create a messages
+        final String text = "Hello world! From: " + Thread.currentThread().getName() + " : " + this.hashCode();
+        TextMessage message = session.createTextMessage(text);
+
+        // Tell the producer to send the message
+        System.out.println("Sent message: " + message.hashCode() + " : " + Thread.currentThread().getName());
+        producer.send(message);
+
+        // Clean up
+        session.close();
+      }
+      catch (final Exception e) {
+        throw new RuntimeException(e);
+      }
+      finally {
+        latch.countDown();
+      }
+    }
+  }
+
+  public static class HelloWorldConsumer implements Runnable, ExceptionListener {
+    private final Connection connection;
+
+    public HelloWorldConsumer(final Connection connection) {
+      this.connection = connection;
+    }
+
+    @Override
+    public void run() {
+      try {
+        // Create a Session
+        final Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+
+        // Create the destination (Topic or Queue)
+        final Destination destination = session.createQueue("TEST.FOO");
+
+        // Create a MessageConsumer from the Session to the Topic or Queue
+        final MessageConsumer consumer = session.createConsumer(destination);
+        System.out.println("CONSUMER: " + consumer);
+
+        // Wait for a message
+        final Message message = consumer.receive(1000);
+        if (message instanceof TextMessage) {
+          final TextMessage textMessage = (TextMessage)message;
+          final String text = textMessage.getText();
+          System.out.println("Received: " + text);
+        }
+        else {
+          System.out.println("Received: " + message);
+        }
+
+        consumer.close();
+        session.close();
+      }
+      catch (final Exception e) {
+        throw new RuntimeException(e);
+      }
+      finally {
+        latch.countDown();
+      }
+    }
+
+    @Override
+    public synchronized void onException(final JMSException e) {
+      System.out.println("JMS Exception occured.  Shutting down client.");
+    }
+  }
+}
---
+++ test/jms-2/src/test/java/io/opentracing/contrib/specialagent/test/jms2/Jms2ITest.java
@@ -0,0 +1,82 @@
+/* Copyright 2019 The OpenTracing Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.opentracing.contrib.specialagent.test.jms2;
+
+import java.io.File;
+import java.util.HashSet;
+
+import javax.jms.Connection;
+import javax.jms.Destination;
+import javax.jms.JMSContext;
+import javax.jms.JMSProducer;
+import javax.jms.MessageConsumer;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+
+import org.apache.activemq.artemis.api.core.TransportConfiguration;
+import org.apache.activemq.artemis.core.config.Configuration;
+import org.apache.activemq.artemis.core.config.impl.ConfigurationImpl;
+import org.apache.activemq.artemis.core.remoting.impl.invm.InVMAcceptorFactory;
+import org.apache.activemq.artemis.core.server.ActiveMQServer;
+import org.apache.activemq.artemis.core.server.impl.ActiveMQServerImpl;
+import org.apache.activemq.artemis.jms.client.ActiveMQJMSConnectionFactory;
+
+import io.opentracing.contrib.specialagent.TestUtil;
+
+public class Jms2ITest {
+  public static void main(final String[] args) throws Exception {
+    final HashSet<TransportConfiguration> transports = new HashSet<>();
+    transports.add(new TransportConfiguration(InVMAcceptorFactory.class.getName()));
+
+    final Configuration configuration = new ConfigurationImpl();
+    configuration.setAcceptorConfigurations(transports);
+    configuration.setSecurityEnabled(false);
+
+    final File targetDir = new File(new File("").getAbsoluteFile(), "target");
+    configuration.setBrokerInstance(targetDir);
+
+    final ActiveMQServer server = new ActiveMQServerImpl(configuration);
+    server.start();
+
+    try (final ActiveMQJMSConnectionFactory connectionFactory = new ActiveMQJMSConnectionFactory("vm://0")) {
+      final Connection connection = connectionFactory.createConnection();
+      connection.start();
+      try (
+        final JMSContext context = connectionFactory.createContext();
+        final Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+      ) {
+        System.out.println("Session: " + session);
+        final Destination destination = session.createQueue("TEST.FOO2");
+
+        final JMSProducer producer = context.createProducer();
+        System.out.println("PRODUCER: " + producer);
+
+        final MessageConsumer consumer = session.createConsumer(destination);
+        System.out.println("CONSUMER: " + consumer);
+
+        final TextMessage message = session.createTextMessage("Hello world");
+        producer.send(destination, message);
+
+        final TextMessage received = (TextMessage)consumer.receive(5000);
+        System.out.println("RECEIVED: " + received.getText());
+      }
+    }
+
+    server.stop(true);
+    TestUtil.checkSpan("java-jms", 2);
+    System.exit(0);
+  }
+}
