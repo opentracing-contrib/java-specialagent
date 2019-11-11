cfa5dd3b4cde82bfa091b2ce3c08c54202d26286
Sergei Malafeev <sergeymalafeev@gmail.com>
2019-11-11 20:47:53 +0800

    #212 Implement AgentRule(s) for Play WS
    Signed-off-by: Sergei Malafeev <sergeymalafeev@gmail.com>

---
+++ test/play-ws-2.0.0/src/test/java/io/opentracing/contrib/specialagent/test/playws/PlayWSITest.java
@@ -0,0 +1,58 @@
+/* Copyright 2019 The OpenTracing Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.opentracing.contrib.specialagent.test.playws;
+
+import akka.actor.ActorSystem;
+import akka.stream.ActorMaterializer;
+import akka.stream.Materializer;
+import io.opentracing.contrib.specialagent.TestUtil;
+import java.util.concurrent.TimeUnit;
+import play.libs.ws.ahc.StandaloneAhcWSClient;
+import play.shaded.ahc.org.asynchttpclient.AsyncHttpClient;
+import play.shaded.ahc.org.asynchttpclient.AsyncHttpClientConfig;
+import play.shaded.ahc.org.asynchttpclient.DefaultAsyncHttpClient;
+import play.shaded.ahc.org.asynchttpclient.DefaultAsyncHttpClientConfig;
+
+public class PlayWSITest {
+  public static void main(final String[] args) throws Exception {
+    TestUtil.initTerminalExceptionHandler();
+    final ActorSystem system = ActorSystem.create();
+    final Materializer materializer = ActorMaterializer.create(system);
+
+    AsyncHttpClientConfig asyncHttpClientConfig =
+        new DefaultAsyncHttpClientConfig.Builder()
+            .setMaxRequestRetry(0)
+            .setShutdownQuietPeriod(0)
+            .setShutdownTimeout(0)
+            .build();
+
+    AsyncHttpClient asyncHttpClient = new DefaultAsyncHttpClient(asyncHttpClientConfig);
+
+    final StandaloneAhcWSClient wsClient = new StandaloneAhcWSClient(asyncHttpClient, materializer);
+
+    final int status = wsClient.url("http://www.google.com").get()
+        .whenComplete((response, throwable) -> {
+          TestUtil.checkActiveSpan();
+        }).toCompletableFuture().get(15, TimeUnit.SECONDS).getStatus();
+    wsClient.close();
+    system.terminate();
+
+    if (200 != status)
+      throw new AssertionError("ERROR: response: " + status);
+
+    TestUtil.checkSpan("play-ws", 1);
+  }
+}
