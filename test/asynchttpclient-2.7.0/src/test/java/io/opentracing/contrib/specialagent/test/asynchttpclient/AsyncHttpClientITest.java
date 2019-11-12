c2139887cafa355f43f8908acd22ad8c32e28475
Sergei Malafeev <sergeymalafeev@gmail.com>
2019-11-12 20:58:52 +0800

    add AsyncHttpClient integration test
    Signed-off-by: Sergei Malafeev <sergeymalafeev@gmail.com>

---
+++ test/asynchttpclient-2.7.0/src/test/java/io/opentracing/contrib/specialagent/test/asynchttpclient/AsyncHttpClientITest.java
@@ -0,0 +1,55 @@
+/* Copyright 2019 The OpenTracing Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.opentracing.contrib.specialagent.test.asynchttpclient;
+
+import io.opentracing.contrib.specialagent.TestUtil;
+import java.util.concurrent.TimeUnit;
+import org.asynchttpclient.AsyncCompletionHandler;
+import org.asynchttpclient.AsyncHttpClient;
+import org.asynchttpclient.DefaultAsyncHttpClient;
+import org.asynchttpclient.Request;
+import org.asynchttpclient.RequestBuilder;
+import org.asynchttpclient.Response;
+import org.asynchttpclient.util.HttpConstants;
+
+public class AsyncHttpClientITest {
+  public static void main(final String[] args) throws Exception {
+    TestUtil.initTerminalExceptionHandler();
+    AsyncHttpClient client = new DefaultAsyncHttpClient();
+
+    Request request = new RequestBuilder(HttpConstants.Methods.GET)
+        .setUrl("http://www.google.com")
+        .build();
+
+    final int statusCode = client.executeRequest(request,
+        new AsyncCompletionHandler<Response>() {
+          @Override
+          public Response onCompleted(Response response) {
+            TestUtil.checkActiveSpan();
+            return response;
+          }
+        }
+    ).get(10, TimeUnit.SECONDS).getStatusCode();
+
+    client.close();
+
+    if (200 != statusCode)
+      throw new AssertionError("ERROR: response: " + statusCode);
+
+    TestUtil.checkSpan("java-asynchttpclient", 1);
+  }
+
+}
