95b7e2b2d6069d6ffa73ac6b7cff338174d366f4
Sergei Malafeev <sergeymalafeev@gmail.com>
2019-11-14 16:35:39 +0800

    add integration tests for gRPC
    Signed-off-by: Sergei Malafeev <sergeymalafeev@gmail.com>

---
+++ test/grpc-1.6.0/src/test/java/io/opentracing/contrib/specialagent/test/grpc/GrpcITest.java
@@ -0,0 +1,60 @@
+/* Copyright 2019 The OpenTracing Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.opentracing.contrib.specialagent.test.grpc;
+
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+import io.grpc.stub.StreamObserver;
+import io.opentracing.contrib.grpc.gen.GreeterGrpc;
+import io.opentracing.contrib.grpc.gen.GreeterGrpc.GreeterBlockingStub;
+import io.opentracing.contrib.grpc.gen.HelloReply;
+import io.opentracing.contrib.grpc.gen.HelloRequest;
+import io.opentracing.contrib.specialagent.TestUtil;
+import java.io.IOException;
+
+public class GrpcITest {
+  public static void main(final String[] args) throws IOException {
+    TestUtil.initTerminalExceptionHandler();
+    final Server server = ServerBuilder.forPort(8086)
+        .addService(new GreeterImpl()).build().start();
+
+    final ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 8086)
+        .usePlaintext(true)
+        .build();
+
+    final GreeterBlockingStub greeterBlockingStub = GreeterGrpc.newBlockingStub(channel);
+
+    greeterBlockingStub.sayHello(HelloRequest.newBuilder().setName("world").build()).getMessage();
+
+    server.shutdownNow();
+
+    TestUtil.checkSpan("java-grpc", 2);
+  }
+
+  private static class GreeterImpl extends GreeterGrpc.GreeterImplBase {
+
+    @Override
+    public void sayHello(HelloRequest req, StreamObserver<HelloReply> responseObserver) {
+      TestUtil.checkActiveSpan();
+      HelloReply reply = HelloReply.newBuilder().setMessage("Hello " + req.getName()).build();
+      responseObserver.onNext(reply);
+      responseObserver.onCompleted();
+    }
+  }
+
+}
