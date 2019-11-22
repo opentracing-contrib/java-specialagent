3ae86e903399c3d2372c874a3c752673dc65162b
Seva Safris <seva@safris.org>
2019-11-22 14:49:58 +0800

    Conform patterns

--- test/lettuce-5.0.0/src/test/java/io/opentracing/contrib/specialagent/test/lettuce/LettuceITest.java
+++ test/lettuce-5.0.0/src/test/java/io/opentracing/contrib/specialagent/test/lettuce/LettuceITest.java
@@ -15,68 +15,69 @@
 
 package io.opentracing.contrib.specialagent.test.lettuce;
 
+import java.lang.reflect.Method;
+
 import io.lettuce.core.RedisClient;
 import io.lettuce.core.api.StatefulRedisConnection;
 import io.lettuce.core.api.sync.RedisCommands;
 import io.opentracing.contrib.specialagent.TestUtil;
-import java.lang.reflect.Method;
 import redis.embedded.RedisServer;
 
 public class LettuceITest {
   public static void main(final String[] args) throws Exception {
     TestUtil.initTerminalExceptionHandler();
-    RedisServer redisServer = new RedisServer();
-    TestUtil.retry(() -> redisServer.start(), 10);
+    final RedisServer redisServer = new RedisServer();
+    TestUtil.retry(redisServer::start, 10);
 
-    RedisClient client = RedisClient.create("redis://localhost");
-    StatefulRedisConnection<String, String> connection = client.connect();
-    RedisCommands<String, String> commands = connection.sync();
-    if (!"OK".equals(commands.set("key", "value"))) {
-      throw new AssertionError("ERROR: failed to set key/value");
-    }
-    if (!"value".equals(commands.get("key"))) {
-      throw new AssertionError("ERROR: failed to get key value");
-    }
+    final RedisClient client = RedisClient.create("redis://localhost");
+    try (final StatefulRedisConnection<String,String> connection = client.connect()) {
+      final RedisCommands<String,String> commands = connection.sync();
+      if (!"OK".equals(commands.set("key", "value")))
+        throw new AssertionError("ERROR: failed to set key/value");
 
-    int spanCount = 2;
+      if (!"value".equals(commands.get("key")))
+        throw new AssertionError("ERROR: failed to get key value");
 
-    // Lettuce 5.1+ has new method 'xlen'
-    final Method xlenMethod = getMethod(commands, "xlen");
-    if (xlenMethod != null) {
-      spanCount++;
-      try {
-        xlenMethod.invoke(commands, "key");
-      } catch (Exception ignore) {
-        // embedded server doesn't support all commands
+      int expectedSpanCount = 2;
+
+      // Lettuce 5.1+ has new method 'xlen'
+      final Method xlenMethod = getMethod(commands, "xlen");
+      if (xlenMethod != null) {
+        ++expectedSpanCount;
+        try {
+          xlenMethod.invoke(commands, "key");
+        }
+        catch (final Exception ignore) {
+          // embedded server doesn't support all commands
+        }
       }
-    }
 
-    // Lettuce 5.2+ has new method 'memoryUsage'
-    final Method memoryUsageMethod = getMethod(commands, "memoryUsage");
-    if (memoryUsageMethod != null) {
-      spanCount++;
-      try {
-        memoryUsageMethod.invoke(commands, "key");
-      } catch (Exception ignore) {
-        // embedded server doesn't support all commands
+      // Lettuce 5.2+ has new method 'memoryUsage'
+      final Method memoryUsageMethod = getMethod(commands, "memoryUsage");
+      if (memoryUsageMethod != null) {
+        ++expectedSpanCount;
+        try {
+          memoryUsageMethod.invoke(commands, "key");
+        }
+        catch (final Exception ignore) {
+          // embedded server doesn't support all commands
+        }
       }
+
+      TestUtil.checkSpan("java-redis", expectedSpanCount);
     }
 
     client.shutdown();
     redisServer.stop();
-    TestUtil.checkSpan("java-redis", spanCount);
-
     // RedisServer process doesn't exit on 'stop' therefore call System.exit
     System.exit(0);
   }
 
-  private static Method getMethod(RedisCommands<String, String> commands, String name) {
-    for (Method method : commands.getClass().getMethods()) {
-      if (method.getName().equals(name)) {
+  private static Method getMethod(final RedisCommands<String,String> commands, final String name) {
+    for (final Method method : commands.getClass().getMethods())
+      if (method.getName().equals(name))
         return method;
-      }
-    }
+
     return null;
   }
-
 }
