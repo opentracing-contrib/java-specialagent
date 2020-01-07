4f6aa0fd057e18e5e3905032795d6cf1fdd6c1ee
Sergei Malafeev <sergeymalafeev@gmail.com>
2020-01-07 22:08:47 +0800

    #321 Implement AgentRule(s) for Neo4J Driver
    Signed-off-by: Sergei Malafeev <sergeymalafeev@gmail.com>

---
+++ rule/neo4j-driver/src/main/java/io/opentracing/contrib/specialagent/rule/neo4j/driver/Neo4jDriverAgentRule.java
@@ -0,0 +1,48 @@
+/* Copyright 2020 The OpenTracing Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.opentracing.contrib.specialagent.rule.neo4j.driver;
+
+import static net.bytebuddy.matcher.ElementMatchers.*;
+
+import java.util.Arrays;
+
+import io.opentracing.contrib.specialagent.AgentRule;
+import net.bytebuddy.agent.builder.AgentBuilder;
+import net.bytebuddy.agent.builder.AgentBuilder.Transformer;
+import net.bytebuddy.asm.Advice;
+import net.bytebuddy.description.type.TypeDescription;
+import net.bytebuddy.dynamic.DynamicType.Builder;
+import net.bytebuddy.implementation.bytecode.assign.Assigner.Typing;
+import net.bytebuddy.utility.JavaModule;
+
+public class Neo4jDriverAgentRule extends AgentRule {
+  @Override
+  public Iterable<? extends AgentBuilder> buildAgent(final AgentBuilder builder) {
+    return Arrays.asList(builder
+      .type(not(isInterface()).and(hasSuperType(named("org.neo4j.driver.GraphDatabase"))))
+      .transform(new Transformer() {
+        @Override
+        public Builder<?> transform(final Builder<?> builder, final TypeDescription typeDescription, final ClassLoader classLoader, final JavaModule module) {
+          return builder.visit(Advice.to(Neo4jDriverAgentRule.class).on(named("driver").and(takesArguments(3)).and(takesArgument(0, named("java.net.URI")))));
+        }}));
+  }
+
+  @Advice.OnMethodExit
+  public static void enter(final @Advice.Origin String origin, @Advice.Return(readOnly = false, typing = Typing.DYNAMIC) Object returned) {
+    if (isEnabled(origin))
+      returned = Neo4jDriverAgentIntercept.exit(returned);
+  }
+}
