aeae580e7505ef7f7f6c5daac23a06ca2364c8e4
Jose Montoya <jam01@protonmail.com>
2020-01-07 21:00:29 -0600

    adds http test

--- test/mule-4.2.2/container-itest/src/test/java/io/opentracing/contrib/specialagent/test/Mule4ContainerITest.java
+++ test/mule-4.2.2/container-itest/src/test/java/io/opentracing/contrib/specialagent/test/Mule4ContainerITest.java
@@ -1,15 +1,20 @@
 package io.opentracing.contrib.specialagent.test;
 
+import io.opentracing.contrib.specialagent.TestUtil;
 import org.mule.runtime.core.api.config.MuleProperties;
 import org.mule.runtime.module.launcher.MuleContainer;
 
 import java.io.File;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class Mule4ContainerITest {
 
     private static final String MULE_HOME = "/src/test/mule-home";
 
     public static void main(String[] args) throws Exception {
+        TestUtil.initTerminalExceptionHandler();
+
         final String homeDir = new File("").getAbsolutePath() + MULE_HOME;
         System.setProperty("mule.simpleLog", "true");
         System.setProperty(MuleProperties.MULE_HOME_DIRECTORY_PROPERTY, homeDir);
@@ -20,10 +25,21 @@ public class Mule4ContainerITest {
                 "-M-Dfile.encoding=UTF-8",
                 "-M-Dmule.timeout.disable=false"});
 
-        container.start(false);
+        try {
+            container.start(false);
 
-        // TODO: 1/7/20 write http request and span assertions
+            final URL obj = new URL("http://localhost:8081");
+            final HttpURLConnection connection = (HttpURLConnection) obj.openConnection();
+            connection.setRequestMethod("GET");
+            int responseCode = connection.getResponseCode();
 
-        container.stop();
+            if (200 != responseCode)
+                throw new AssertionError("ERROR: response: " + responseCode);
+
+            TestUtil.checkSpan("java-grizzly-ahc", 2);
+            TestUtil.checkSpan("java-grizzly-http-server", 2);
+        } finally {
+            container.stop();
+        }
     }
 }
