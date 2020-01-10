78da9adae6f947be2cc6f9e7b5bece302b4eb329
Jose Montoya <jam01@protonmail.com>
2020-01-10 11:17:41 -0600

    add waits for runtime

--- test/mule-4.2.2/container-itest/src/test/java/io/opentracing/contrib/specialagent/test/Mule4ContainerITest.java
+++ test/mule-4.2.2/container-itest/src/test/java/io/opentracing/contrib/specialagent/test/Mule4ContainerITest.java
@@ -4,6 +4,7 @@ import io.opentracing.contrib.specialagent.TestUtil;
 import io.opentracing.mock.MockSpan;
 import io.opentracing.mock.MockTracer;
 import io.opentracing.tag.Tags;
+import org.mule.runtime.api.util.MuleSystemProperties;
 import org.mule.runtime.core.api.config.MuleProperties;
 import org.mule.runtime.module.launcher.MuleContainer;
 
@@ -14,12 +15,12 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 public class Mule4ContainerITest {
-
     private static final String MULE_HOME = "/src/test/mule-home";
 
     public static void main(String[] args) throws Exception {
         final String homeDir = new File("").getAbsolutePath() + MULE_HOME;
-        System.setProperty("mule.simpleLog", "true");
+        final Mule4ContainerITest lock = new Mule4ContainerITest();
+        System.setProperty(MuleSystemProperties.MULE_SIMPLE_LOG, "true");
         System.setProperty(MuleProperties.MULE_HOME_DIRECTORY_PROPERTY, homeDir);
         System.setProperty(MuleProperties.MULE_BASE_DIRECTORY_PROPERTY, homeDir);
         MuleContainer container = new MuleContainer(new String[]{
@@ -31,11 +32,21 @@ public class Mule4ContainerITest {
         try {
             container.start(false);
 
+            // giving the runtime time to make the http listener available
+            synchronized (lock) {
+                lock.wait(200);
+            }
+
             final URL obj = new URL("http://localhost:8081");
             final HttpURLConnection connection = (HttpURLConnection) obj.openConnection();
             connection.setRequestMethod("GET");
             int responseCode = connection.getResponseCode();
             connection.disconnect();
+
+            // giving grizzly-http-server time to finish the span, since it's on a diff thread
+            synchronized (lock) {
+                lock.wait(200);
+            }
 
             if (200 != responseCode)
                 throw new AssertionError("ERROR: response: " + responseCode);
@@ -43,13 +54,25 @@ public class Mule4ContainerITest {
             checkSpans("java-grizzly-http-server", "http:request", "java-grizzly-ahc", "mule:logger");
         } finally {
             container.stop();
+            System.clearProperty(MuleSystemProperties.MULE_SIMPLE_LOG);
+            System.clearProperty(MuleProperties.MULE_HOME_DIRECTORY_PROPERTY);
+            System.clearProperty(MuleProperties.MULE_BASE_DIRECTORY_PROPERTY);
         }
     }
 
     private static void checkSpans(String... components) {
         MockTracer mockTracer = (MockTracer) TestUtil.getGlobalTracer();
+        System.out.println("Spans: " + mockTracer.finishedSpans());
         Map<String, MockSpan> finishedSpans = mockTracer.finishedSpans()
                 .stream()
+                .peek(mockSpan -> {
+                    System.out.println("Span: " + mockSpan);
+                    System.out.println("\tComponent: " + mockSpan.tags().get(Tags.COMPONENT.getKey()));
+                    System.out.println("\tTags: " + mockSpan.tags());
+                    System.out.println("\tLogs: ");
+                    for (final MockSpan.LogEntry logEntry : mockSpan.logEntries())
+                        System.out.println("\t" + logEntry.fields());
+                })
                 .collect(Collectors.toMap(mockSpan -> (String) mockSpan.tags().get(Tags.COMPONENT.getKey()),
                         mockSpan -> mockSpan));
 
