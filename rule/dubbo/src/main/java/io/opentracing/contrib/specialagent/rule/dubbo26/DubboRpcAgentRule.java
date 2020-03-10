b22c2378899bb850f6efae13ef28e3c608418681
jianwei.xujw <jianwei.xujw@alibaba-inc.com>
2020-03-10 16:07:35 +0800

    add appache dubbo rpc support to #issue 475

---
+++ rule/dubbo/src/main/java/io/opentracing/contrib/specialagent/rule/dubbo26/DubboRpcAgentRule.java
@@ -0,0 +1,74 @@
+/* Copyright 2018 The OpenTracing Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.opentracing.contrib.specialagent.rule.dubbo26;
+
+import static net.bytebuddy.matcher.ElementMatchers.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import com.alibaba.dubbo.rpc.Filter;
+import io.opentracing.contrib.specialagent.AgentRule;
+import net.bytebuddy.agent.builder.AgentBuilder;
+import net.bytebuddy.agent.builder.AgentBuilder.Transformer;
+import net.bytebuddy.asm.Advice;
+import net.bytebuddy.description.type.TypeDescription;
+import net.bytebuddy.dynamic.DynamicType.Builder;
+import net.bytebuddy.implementation.bytecode.assign.Assigner.Typing;
+import net.bytebuddy.utility.JavaModule;
+
+public class DubboRpcAgentRule extends AgentRule {
+  @Override
+  public Iterable<? extends AgentBuilder> buildAgent(final AgentBuilder builder) throws Exception {
+    return Arrays.asList(builder
+      .type(named("com.alibaba.dubbo.common.extension.ExtensionLoader"))
+      .transform(new Transformer() {
+        @Override
+        public Builder<?> transform(final Builder<?> builder, final TypeDescription typeDescription, final ClassLoader classLoader, final JavaModule module) {
+          return builder.visit(Advice.to(DubboRpcAgentRule.class).on(named("getActivateExtension")
+                  .and(takesArguments(3)).and(returnsGeneric(Filter.class.getComponentType()))));
+        }}));
+  }
+
+    @Advice.OnMethodExit
+    public static void exit(final @Advice.Origin String origin,@Advice.Argument(value = 1, readOnly = false, typing = Typing.DYNAMIC) String  key,
+                            @Advice.Return(readOnly = false, typing = Typing.DYNAMIC) List<Filter> returned) {
+        if ("service.filter".equals(key)) {
+            if (isEnabled("DubboRpcAgentRule", origin)) {
+                returned = exit(returned);
+            }
+        } else if ("reference.filter".equals(key)) {
+            if (isEnabled("DubboRpcAgentRule", origin)) {
+                returned = exit(returned);
+            }
+        }
+    }
+
+    public static List<Filter>  exit(final List<Filter> filters) {
+
+        for (final Filter filter : filters) {
+            if (filter instanceof DubboRpcFilter) {
+                return filters;
+            }
+        }
+
+        final ArrayList<Filter> newFilters = new ArrayList<Filter>(filters);
+        final DubboRpcFilter filter = new DubboRpcFilter();
+        newFilters.add(filter);
+        return newFilters;
+    }
+}
