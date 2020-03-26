a075dd9beed9fe6abfebf4174ed9f9b337aeb263
Seva Safris <seva@safris.org>
2020-03-26 14:55:35 +0700

    Conform patterns, re #510

--- rule/apache-httpclient/src/test/java/io/opentracing/contrib/specialagent/rule/apache/httpclient/ConfigurationTest.java
+++ rule/apache-httpclient/src/test/java/io/opentracing/contrib/specialagent/rule/apache/httpclient/ConfigurationTest.java
@@ -1,16 +1,31 @@
+/* Copyright 2020 The OpenTracing Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.opentracing.contrib.specialagent.rule.apache.httpclient;
 
 import static org.junit.Assert.*;
+
 import java.util.Arrays;
 import java.util.List;
+
 import org.junit.Test;
 
 public class ConfigurationTest {
-
   @Test
   public void testExplicitSpanDecorators() {
-    testDecorators(ApacheClientSpanDecorator.StandardTags.class.getName() + "," + MockSpanDecorator.class.getName(),
-        ApacheClientSpanDecorator.StandardTags.class, MockSpanDecorator.class);
+    testDecorators(ApacheClientSpanDecorator.StandardTags.class.getName() + "," + MockSpanDecorator.class.getName(), ApacheClientSpanDecorator.StandardTags.class, MockSpanDecorator.class);
   }
 
   @Test
@@ -18,7 +33,7 @@ public class ConfigurationTest {
     testDecorators(null, ApacheClientSpanDecorator.StandardTags.class);
   }
 
-  private static void testDecorators(final String spanDecoratorsArgs, final Class<?>... expecteds) {
+  private static void testDecorators(final String spanDecoratorsArgs, final Class<?> ... expecteds) {
     final List<ApacheClientSpanDecorator> decorators = Configuration.parseSpanDecorators(spanDecoratorsArgs);
     assertEquals(expecteds.length, decorators.size());
     final List<Class<?>> list = Arrays.asList(expecteds);
