d883d7cff2ad6b8faa443fd3b9bef8be096f070c
jianwei.xujw <jianwei.xujw@alibaba-inc.com>
2020-03-27 13:47:39 +0800

    add some comments

--- rule/dubbo-2.7/src/main/java/io/opentracing/contrib/specialagent/rule/dubbo27/DubboAgentRule.java
+++ rule/dubbo-2.7/src/main/java/io/opentracing/contrib/specialagent/rule/dubbo27/DubboAgentRule.java
@@ -1,3 +1,17 @@
+/* Copyright 2020 The OpenTracing Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.opentracing.contrib.specialagent.rule.dubbo27;
 
 import io.opentracing.contrib.specialagent.AgentRule;
@@ -7,11 +21,8 @@ import net.bytebuddy.description.type.TypeDescription;
 import net.bytebuddy.dynamic.DynamicType;
 import net.bytebuddy.implementation.bytecode.assign.Assigner;
 import net.bytebuddy.utility.JavaModule;
-import org.apache.dubbo.rpc.Filter;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 import static net.bytebuddy.matcher.ElementMatchers.named;
 
