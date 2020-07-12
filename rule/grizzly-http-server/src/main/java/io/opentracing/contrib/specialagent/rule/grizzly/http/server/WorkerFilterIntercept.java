d3bfd68357f3f1874d565ff832fd395146b095b0
Jose Montoya <jam01@protonmail.com>
2020-07-12 00:04:00 -0500

    grizzly-http-server: minor cleanup

--- rule/grizzly-http-server/src/main/java/io/opentracing/contrib/specialagent/rule/grizzly/http/server/WorkerFilterIntercept.java
+++ rule/grizzly-http-server/src/main/java/io/opentracing/contrib/specialagent/rule/grizzly/http/server/WorkerFilterIntercept.java
@@ -2,18 +2,7 @@ package io.opentracing.contrib.specialagent.rule.grizzly.http.server;
 
 import io.opentracing.Scope;
 import io.opentracing.Span;
-import io.opentracing.SpanContext;
-import io.opentracing.Tracer;
-import io.opentracing.propagation.Format;
-import io.opentracing.propagation.TextMap;
-import io.opentracing.tag.Tags;
 import io.opentracing.util.GlobalTracer;
-import org.glassfish.grizzly.filterchain.FilterChainContext;
-import org.glassfish.grizzly.filterchain.NextAction;
-import org.glassfish.grizzly.http.HttpContent;
-import org.glassfish.grizzly.http.HttpHeader;
-import org.glassfish.grizzly.http.HttpRequestPacket;
-import org.glassfish.grizzly.http.HttpResponsePacket;
 
 public class WorkerFilterIntercept {
     public static Scope onHandleReadEnter(
